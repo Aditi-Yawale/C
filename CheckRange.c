7e4652281c515da6264c8cbd5f34fbfd01c5d2ba
Aditi Yawale <yawaleaditi@gmail.com>
2024-08-10 19:50:41 +0530

    Add files via upload
    Check if a given number is in the range 30 to 50

---
+++ CheckRange.c
@@ -0,0 +1,46 @@
+//Check if a given number is in the range 30 to 50
+
+#include<stdio.h>
+#include<stdbool.h>
+
+/*
+    START
+        Accept one number as No
+        If the number is greater than 30 and if it is lessmthan 50
+        then display the message as number is in range
+        otherwise
+        display the number is not in the range
+    STOP
+*/
+bool CheckRange(int iNo)
+{
+    if((iNo >=30) && (iNo <=50))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    printf("Enter number : \n");
+    scanf("%d", &iValue);
+
+    bRet = CheckRange(iValue);
+    if(bRet == true)
+    {
+        printf("%d is in the range 30 & 50\n", iValue);
+    }
+    else
+    {
+        printf("%d is in NOT the range 30 & 50\n", iValue);
+    }
+    return 0;
+}
